47d84fbcfbefe77a317b574a7d99d12007a1857f
Ricardo Andrés Penilla Muñoz <riichardmunoz@gmail.com>
2018-06-27 18:14:14 -0500

    Part14

---
+++ Lands/Lands/ViewModels/LandItemViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Lands.ViewModels
+{
+    using Lands.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class LandItemViewModel: Land
+    {
+
+    }
+}
